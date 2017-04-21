a5e804914be36d9fc1aa73c47b9371be0adc0dec
asbarve <adityasbarve@gmail.com>
2017-04-21 18:18:17 -0400

    added timer

--- js/countdown.js
+++ js/countdown.js
@@ -6,23 +6,25 @@ var countDownDate = new Date("June 1, 2017 12:00:00").getTime();
 var x = setInterval(function() {
 
   // Get todays date and time
-  var now = new Date().getTime();
+  var today = new Date().getTime();
 
   // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+  var launchDate = countDownDate - today;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(launchDate / (1000 * 60 * 60 * 24));
+  var hours = Math.round((launchDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.round((launchDate % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.round((launchDate % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML =  hours;
+  document.getElementById("minutes").innerHTML =  minutes;
+  document.getElementById("seconds").innerHTML =  seconds;
 
   // If the count down is finished, write some text
-  if (distance < 0) {
+  if (launchDate < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
